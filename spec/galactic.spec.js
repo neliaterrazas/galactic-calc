83b0d473a01dfcd705749b8dd17389a9c59edda3
Student <example@example.com>
2019-03-15 09:28:00 -0700

    create first functions and start running tests

--- spec/galactic.spec.js
+++ spec/galactic.spec.js
@@ -1,1 +1,11 @@
 import { todaysDate } from "../src/galactic-back-end.js";
+import { calculateAge } from "../src/galactic-back-end.js";
+
+
+describe ("what day is it?", function() {
+  it('should tell me todays date', function(){
+    const date = new todaysDate();
+    expect(todaysDate.toString()).toEqual("date");
+
+  });
+});
--- src/galactic-back-end.js
+++ src/galactic-back-end.js
@@ -2,3 +2,25 @@ export function todaysDate (year, month, day) {
   const date = new Date(year, month, day);
   return date;
 }
+ export function calculateAge(dateString) {
+  const today = new Date();
+  const birthday = new Date(dateString);
+  let age = today.getFullYear() - birthday.getFullYear();
+  const month = today.getMonth  - birthday.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthday.getDate())) {
+    age--;
+  }
+  return age;
+}
+export function mercury(){
+
+}
+export function venus() {
+
+}
+export function mars() {
+
+}
+export function jupiter() {
+
+}
