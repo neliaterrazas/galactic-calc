7963d01d946c589a66433e51a2793a92bc1e761d
Student <example@example.com>
2019-03-15 08:41:39 -0700

    npm install & npm run build for webpack, karma/jasmine

---
+++ spec/galactic.spec.js
@@ -0,0 +1,1 @@
+import { todaysDate } from "../src/galactic-back-end.js";
---
+++ src/galactic-back-end.js
@@ -0,0 +1,4 @@
+export function todaysDate (year, month, day) {
+  const date = new Date(year, month, day);
+  return date;
+}
