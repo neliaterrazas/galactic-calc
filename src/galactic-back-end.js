1b34a32de7e655038a06a6390fe2e046f6c9ce57
Nelia Terrazas <terrazasnelia@gmail.com>
2019-03-22 08:48:43 -0700

    refactor ES6 classes

--- src/galactic-back-end.js
+++ src/galactic-back-end.js
@@ -1,41 +1,47 @@
-export function thisYear() {
-  const date = new Date();
-  const year = date.getFullYear();
-  return year;
-}
+// export function thisYear() {
+//   const date = new Date();
+//   const year = date.getFullYear();
+//   return year;
+// }
 
-export function getAge() {
-    let today = new Date();
-    let birthDate = new Date();
+
+
+export class galacticCalc() {
+  constructor(){
+    this.yourAge = userAge;
+    this.mercuryAge = mercuryAge;
+  }
+
+  getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
     let age = today.getFullYear() - birthDate.getFullYear();
-    let m = today.getMonth() - birthDate.getMonth();
-
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
-    age --;
-      return age.toDateString();
+    const month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
+    {
+      age--;
     }
+    return age;
+  }
+  seconds(age) {
+    const ageInSeconds = age * 31557600;
+    return ageInSeconds;
+  }
 
-
-
-export function seconds(age) {
-  const ageInSeconds = age * 31557600;
-  return ageInSeconds;
-}
-
-
-export function mercury(age){
-  const mercuryAge = age * 0.24;
-  return mercuryAge;
-}
-export function venus(age){
-  const venusAge = age * 0.62;
-  return venusAge;
-}
-export function mars(age){
-  const marsAge = age * 1.88;
-  return marsAge;
-}
-export function jupiter(age){
-  const jupiterAge = age * 11.86;
-  return jupiterAge;
+  mercury(age){
+    const mercuryAge = age * 0.24;
+    return mercuryAge;
+  }
+  venus(age){
+    const venusAge = age * 0.62;
+    return venusAge;
+  }
+  mars(age){
+    const marsAge = age * 1.88;
+    return marsAge;
+  }
+  jupiter(age){
+    const jupiterAge = age * 11.86;
+    return jupiterAge;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import $ from 'jquery';
-import { thisYear } from "../src/galactic-back-end.js";
-import { seconds } from "../src/galactic-back-end.js";
-import { getAge } from "../src/galactic-back-end.js";
-import { mercury } from "../src/galactic-back-end.js";
-import { venus } from "../src/galactic-back-end.js";
-import { mars } from "../src/galactic-back-end.js";
-import { jupiter } from "../src/galactic-back-end.js";
+import { galacticCalc } from "../src/galactic-back-end.js";
+
 
 $(document).ready(function(){
   $('#calendar').submit(function(event){
